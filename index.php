c9609d431e9574db0abaa9ecfdecf5a25ee99157
Sofia Moreno <morenosanchezlaurasofia@gmail.com>
2021-11-02 20:45:01 -0500

    inicio

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
-<<<<<<< HEAD
+
 $arreglo= array('envio' =>"Sofia" , "mensaje"=>"se ha enviado" );
 echo json_encode($arreglo);
 ?>
-=======
-echo "welcome to services s";
-?>
->>>>>>> c9fb7f4ad637c6852918e685ab0a54f4dca10fee
+
