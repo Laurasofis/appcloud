89c9538ee2ce619f1cba07d11bf05883dfe01d11
Sofia Moreno <morenosanchezlaurasofia@gmail.com>
2021-11-04 18:08:07 -0500

    inicio

--- servicio1.php
+++ servicio1.php
@@ -5,7 +5,7 @@ require_once __DIR__ . "/vendor/autoload.php";
 $client = new MongoDB\Client(
     'mongodb+srv://sofia:admin1234@cluster0.dzkjr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 
-$tb = $client->datos->usuarios;
+/*$tb = $client->datos->usuarios;
 
 $document = array(
     "name" => $_POST["name"],
@@ -19,6 +19,19 @@ if (count($cursor->toArray())>0){
     echo"Bienvenido";
 }else{
     echo "No";
-}
+}*/
+
+$tb=$client->Lluvia->Precipitacion;
+$filter= ['$and'=>
+            [
+                ['ANNO'=>['$eq'=>'1997']],
+                ['ESTACION'=>['$eq'=>'Sutatausa']]
+            ]
+        ];
+//$query = new MongoDB\Driver\Query($filter);
+$rows = $tb->find($filter);
+$datos= iterator_to_array($rows);
+echo json_encode($datos);
+
 
 ?>
